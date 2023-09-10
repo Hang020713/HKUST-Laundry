fbfa486793b445ced9b305911b499da06a0b999d
Hang020713 <126497819+Hang020713@users.noreply.github.com>
2023-09-11 00:13:33 +0800

    Build 75%

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ elif("1" == target):
 '''
 
 istc_id = 'whchanbi@connect.ust.hk'
-target = "Washing Machine"
+target = "Dry Machine"
 password ='Hkust61387159'
 
 def sendEmail(code_path):
@@ -82,7 +82,8 @@ time.sleep(5)
 
 #Get Tickets
 print('Looking for tickets')
-tmp = driver.find_elements(By.CLASS_NAME, 'segment')[0].find_elements(By.TAG_NAME, 'div')[1]
+tmp = driver.find_elements(By.CLASS_NAME, 'segment')[0]
+tmp = tmp.find_elements(By.TAG_NAME, 'div')[1]
 tickets = tmp.find_elements(By.TAG_NAME, 'small')
 for ticket in tickets:
     if target in ticket.text:
@@ -97,49 +98,55 @@ tmp.click()
 time.sleep(5)
 
 #Get Target Status
-tmp = driver.find_element(By.CLASS_NAME, 'segment').find_elements(By.TAG_NAME, 'div')[1].find_elements(By.TAG_NAME, 'div')[0]
-machines = tmp.find_elements(By.TAG_NAME, 'small')
-ava_machines = []
-for machine in machines:
-    if machine.text == "Available":
-        print("Found Available: ", tmp.text)
-        ava_machines.append(tmp)
-    tmp = machine  #As last record
+while True:
+    tmp = driver.find_element(By.CLASS_NAME, 'segment').find_elements(By.TAG_NAME, 'div')[1].find_elements(By.TAG_NAME, 'div')[0]
+    machines = tmp.find_elements(By.TAG_NAME, 'small')
+    ava_machines = []
+    for machine in machines:
+        if machine.text == "Available":
+            print("Found Available: ", tmp.text)
+            ava_machines.append(tmp)
+        tmp = machine  #As last record
 
-print("Now ", len(ava_machines), " avaiable(s) machine")
-for machine in ava_machines:
-    print(machine.text)
+    '''
+    print("Now ", len(ava_machines), " avaiable(s) machine")
+    for machine in ava_machines:
+        print(machine.text)
+    '''
 
-#Click avaiable
-if ava_machines.__len__ > 0:
-    #Have avaiable, click first
-    ava_machines[0].click()
+    #Click avaiable
+    if len(ava_machines) > 0:
+        #Have avaiable, click first
+        ava_machines[0].click()
 
-    #Activite 1 button
-    for button in driver.find_elements(By.CLASS_NAME, 'button'):
-        for p in button.find_elements(By.TAG_NAME, 'p'):
-            if p.text == "Activate Machine":
+        #Activite 1 button
+        for button in driver.find_elements(By.CLASS_NAME, 'button'):
+            for p in button.find_elements(By.TAG_NAME, 'p'):
+                if p.text == "Activate Machine":
+                    button.click()
+        time.sleep(0.5)
+        
+        #Activite 2 button
+        for button in driver.find_elements(By.CLASS_NAME, 'button'):
+            if button.text == "Proceed":
                 button.click()
-    time.sleep(0.5)
-    
-    #Activite 2 button
-    for button in driver.find_elements(By.CLASS_NAME, 'button'):
-        if button.text == "Proceed":
-            button.click()
 
-    #Capture QR Code
-    code_path = 1
+        #Capture QR Code
+        code_path = 1
 
-    for file_path in os.listdir(os.getcwd()):
-        if os.path.isfile(file_path):
-            if file_path == code_path + ".png":
-                code_path += 1
-    code_path = os.getcwd() + "\\" + code_path + ".png"
-    driver.find_element(By.TAG_NAME, 'body').screenshot(code_path)
-    sendEmail(code_path)
+        for file_path in os.listdir(os.getcwd()):
+            if os.path.isfile(file_path):
+                if file_path == code_path + ".png":
+                    code_path += 1
+        code_path = os.getcwd() + "\\" + code_path + ".png"
+        driver.find_element(By.TAG_NAME, 'body').screenshot(code_path)
+        sendEmail(code_path)
 
-    #Delete the QR Code
-    os.remove(code_path)
-    print('Finish Booking!')
-else:
-    print('No machine avaiable')
+        #Delete the QR Code
+        os.remove(code_path)
+        print('Finish Booking!')
+        break
+    else:
+        driver.refresh()
+        time.sleep(1)
+        print('No machine avaiable')
